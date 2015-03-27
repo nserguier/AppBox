e49fb337dfd443abce6eac61672ca7ac269308bd
ShuBuster <nickyshadow34@hotmail.com>
2015-03-27 15:05:24 +0100

    Glow

--- src/com/Atlas/framework/MenuActivity.java
+++ src/com/Atlas/framework/MenuActivity.java
@@ -25,12 +25,14 @@ public class MenuActivity extends Activity {
 		Button b3 = (Button) findViewById(R.id.bouton3);
 		Button b4 = (Button) findViewById(R.id.bouton4);
 		Button b5 = (Button) findViewById(R.id.bouton5);
+		Button b6 = (Button) findViewById(R.id.bouton6);
 		
 		b1.setText("Drag and Drop");
 		b2.setText("Text to Speech");
 		b3.setText("Boutons");
 		b4.setText("Menus");
 		b5.setText("Plein écran");
+		b6.setText("Glow");
 		
 		/* Cree les lien des boutons */
 		b1.setOnClickListener(new NextActivityListener(this,b1,MenuActivity.this,DragAndDropActivity.class));
@@ -38,6 +40,7 @@ public class MenuActivity extends Activity {
 		b3.setOnClickListener(new NextActivityListener(this,b3,MenuActivity.this,BoutonsActivity.class));
 		b4.setOnClickListener(new NextActivityListener(this,b4,MenuActivity.this,CustomMenuActivity.class));
 		b5.setOnClickListener(new NextActivityListener(this,b5,MenuActivity.this,FullScreenActivity.class));
+		b6.setOnClickListener(new NextActivityListener(this,b6,MenuActivity.this,GlowActivity.class));
 	}
 	
 	@Override
@@ -49,12 +52,14 @@ public class MenuActivity extends Activity {
 		Button b3 = (Button) findViewById(R.id.bouton3);
 		Button b4 = (Button) findViewById(R.id.bouton4);
 		Button b5 = (Button) findViewById(R.id.bouton5);
+		Button b6 = (Button) findViewById(R.id.bouton6);
 		Drawable d = getResources().getDrawable(R.drawable.bouton_bleu);
 		b1.setBackground(d);
 		b2.setBackground(d);
 		b3.setBackground(d);
 		b4.setBackground(d);
 		b5.setBackground(d);
+		b6.setBackground(d);
 
 	}
 }
