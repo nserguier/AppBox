2b69a689e06ee70c30a4464990581d995220cdd3
ShuBuster <nickyshadow34@hotmail.com>
2015-03-28 17:03:01 +0100

    Glow animé de texte et de boutons

--- src/boutons/ButtonCreator.java
+++ src/boutons/ButtonCreator.java
@@ -5,17 +5,43 @@ import com.Atlas.framework.R;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class ButtonCreator {
 
+	/**
+	 * Cree un bouton bleu classique avec un texte donne
+	 * @param c Le contexte de l'activite
+	 * @param nomBouton Le texte a mettre sur le bouton
+	 * @return Le bouton cree
+	 */
 	public static Button createBlueButton(Context c,String nomBouton){
 		Button b = new Button(c);
 		b.setBackground(c.getResources().getDrawable(R.drawable.bouton_bleu));
 		b.setLayoutParams(new LinearLayout.LayoutParams(400, 150));
 		b.setText(nomBouton);
-		b.setTextColor(c.getResources().getColor(R.color.fushia));
+		b.setTextColor(c.getResources().getColor(R.color.jaune1));
 		b.setPadding(0, 0, 0, 10);
 		return b;
+	}
+	
+	/**
+	 * Change l'apparence d'un bouton pour lui donner clele du bouton bleu classique
+	 * @param c Le contexte de l'activite
+	 * @param b Le bouton a modifier
+	 * @param nomBouton Le texte a mettre sur le bouton
+	 */
+	public static void setBlueButton(Context c,Button b, String nomBouton){
+		b.setBackground(c.getResources().getDrawable(R.drawable.bouton_bleu_w));
+		RelativeLayout.LayoutParams params =  (LayoutParams) b.getLayoutParams();
+		params.height = 200;
+		params.width = 500;
+		b.setLayoutParams(params);
+		b.setText(nomBouton);
+		b.setTextSize(30f);
+		b.setTextColor(c.getResources().getColor(R.color.jaune1));
+		b.setPadding(0, 0, 0, 15);
 	}
 	
 }
--- src/com/Atlas/framework/GlowActivity.java
+++ src/com/Atlas/framework/GlowActivity.java
@@ -1,15 +1,42 @@
 package com.Atlas.framework;
 
+import boutons.ButtonCreator;
+import glow.GlowingText;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GlowActivity extends Activity {
 
+	float 	startGlowRadius = 3f,
+			minGlowRadius   = 3f,
+			maxGlowRadius   = 20f;
+	GlowingText glowText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_glow);
+		
+		TextView textView = (TextView) findViewById(R.id.glow);
+		int color = getResources().getColor(R.color.bleu2);
+		glowText = new GlowingText(this,getBaseContext(),textView,minGlowRadius,maxGlowRadius,startGlowRadius,color,1);
+		
+		Button bouton = (Button) findViewById(R.id.glow_bouton);
+		ButtonCreator.setBlueButton(this,bouton, "glowing button");
+		
+		Button bouton2 = (Button) findViewById(R.id.glow_bouton_2);
+		ButtonCreator.setBlueButton(this,bouton2, "glowing button 2");
+		
+		ImageView img = (ImageView) findViewById(R.id.glow_shadow);
+		img.startAnimation(AnimationUtils.loadAnimation(this, R.anim.glow_scale));
+		
+		ImageView img2 = (ImageView) findViewById(R.id.glow_shadow_2);
+		img2.startAnimation(AnimationUtils.loadAnimation(this, R.anim.glow_scale));
+		
 	}
 }
